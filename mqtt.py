1108c282deb4bc0de35751cc185468c3c169a80f
Rob Penn <rjp@ecs.soton.ac.uk>
2018-08-14 11:26:03 +0100

    baked mqtt interface into image_card process. changed mqtt posting frequency from 2 to 5 sec. added hostname and topic_id config options to config file.

--- mqtt.py
+++ mqtt.py
@@ -2,7 +2,7 @@ import threading
 import time
 import random
 import paho.mqtt.client as mqtt
-import ptydash.cards.image_card as graph
+#import ptydash.cards.image_card as graph
 
 # mqtt broker
 host = "wonderbox.ecs.soton.ac.uk"
@@ -12,17 +12,17 @@ client_name = "IAMACLIENT"
 
 logFile = "log.txt"
 
-def on_message(client,userdata,message):
-    #print("message topic=", message.topic)
-    #print("message received ", str(message.payload.decode("utf-8")))
-    # print("message qos=", message.qos)
-    # print("message retain flag=", message.retain)
-    outMsg = str(message.payload.decode("utf-8"))
-    graph.data = outMsg
-    return
+#def on_message(client,userdata,message):
+    ##print("message topic=", message.topic)
+    ##print("message received ", str(message.payload.decode("utf-8")))
+    ## print("message qos=", message.qos)
+    ## print("message retain flag=", message.retain)
+    #outMsg = str(message.payload.decode("utf-8"))
+    #graph.data = outMsg
+    #return
 
 client = mqtt.Client(client_name)
-client.on_message = on_message
+#client.on_message = on_message
 client.connect(host, port=1883, keepalive=60, bind_address="")
 
 def send_mqtt():
@@ -35,11 +35,12 @@ def send_mqtt():
         temperatureReading = random.randint(0,100)
         humidityReading = random.randint(0,100)
         tempAndHumid = "Temperature: " + str(temperatureReading) + " Humidity: " + str(humidityReading)
+        print("sending data "+"Temperature: " + str(temperatureReading) + " Humidity: " + str(humidityReading))
 
         #example publish setting
         client.publish("datadump/tempAndHumid",tempAndHumid)
 
-        time.sleep(2)
+        time.sleep(5)
     return
 
 def write_data():
--- ptydash/cards/image_card.py
+++ ptydash/cards/image_card.py
@@ -2,51 +2,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 import io
 import paho.mqtt.client as mqtt
+import random
 
 import ptydash.interface
 
 class ImageCard(ptydash.interface.Card):#
 
-    def __init__(self, id, text=None, update_delay=1000, **kwargs):
+    def __init__(self, id, text=None, update_delay=1000, hostname=None, topic_id=None, **kwargs):
         super(ImageCard, self).__init__(id, text, update_delay)
         self.data = None
-        # mqtt data stream connection
-        # mqtt broker hostname
-        host = "wonderbox.ecs.soton.ac.uk"
-        # TODO add host config entry to config file
+        self.Xdatastorage = []
+        self.Ydatastorage = []
+        self.clientlist = []
+        # mqtt data stream connection info
+        self.host = hostname
+        self.topicID = topic_id
 
-        # mqtt client id. Change this to something unique.
-        self.client_name = "IAMACLIENT"
-        self.client = mqtt.Client(self.client_name)
-        self.client.on_message = self.on_message
-        self.client.on_connect = self.on_connect
-        self.client.connect(host, port=1883, keepalive=60, bind_address="")
-        self.client.subscribe("datadump/tempAndHumid")
-        self.client.loop_start()
-        # TODO add MQTT topic subscription field to config file
+        identity = random.randint(0, 100)
+        self.client_name = "PTYDASHCLIENT" + str(identity)
+        self.clientlist.append(self.client_name)
+        self.client_create(self.client_name)
 
-    def on_connect(self, client, userdata, flags, rc):
-        print(flags, rc)
+    def client_create(self, clientname):
+        for x in self.clientlist:
+            if x == clientname:
+                print("this is my clientname " + clientname)
+                self.mqtt_subscribe(self.client_name, self.host, self.topicID)
 
+    def mqtt_subscribe(self, client_name, hostname, topic):
+        client = mqtt.Client(client_name)
+        client.on_message = self.on_message
+        client.connect(hostname, port=1883, keepalive=60, bind_address="")
+        client.loop_start()
+        client.subscribe(topic)
+
+    # message receiver
     def on_message(self, client, userdata, message):
-        print("message topic=", message.topic)
-        # print("message received ", str(message.payload.decode("utf-8")))
-        # print("message qos=", message.qos)
-        # print("message retain flag=", message.retain)
+        #print("message topic=", message.topic)
+        #print("message received ", str(message.payload.decode("utf-8")))
+        #print("message qos=", message.qos)
+        #print("message retain flag=", message.retain)
         outMsg = str(message.payload.decode("utf-8"))
         self.data = outMsg
         return
 
     # graph generation
-
     def get_graph(self):
-        Xdatastorage = []
-        Ydatastorage = []
         # data management
-        #data = self.on_message().outMsg#
         if self.data == None:
             return
-        print("this is the data: " + self.data)
+        print("data recieved: " + self.data)
 
         # split mqtt message stream into manageable chunks
         # header:, value, header:, value
@@ -56,15 +61,15 @@ class ImageCard(ptydash.interface.Card):#
         print datalist[2] + datalist[3]
 
         # add the data values to a list to populate a graph once the length of the list is equal to 10 data points
-        Xdatastorage.append(datalist[1])
-        Ydatastorage.append(datalist[3])
+        self.Xdatastorage.append(datalist[1])
+        self.Ydatastorage.append(datalist[3])
 
         # create the graph: using matplotlib.pyplot
         # set graph info before plotting/scattering
         plt.axis([0, 100, 0, 100])  # fix axis scale 0-100
         plt.xlabel(datalist[0])  # set xlabel to header 1
         plt.ylabel(datalist[2])  # set ylabel to header 2
-        plt.scatter([Xdatastorage], [Ydatastorage], marker='s')
+        plt.scatter([self.Xdatastorage], [self.Ydatastorage], marker='s')
 
         buffer = io.BytesIO()
         plt.savefig(buffer, format='png')
