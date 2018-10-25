50413eb768163b6bd1e3029babcab3f6d8b2e34d
James Graham <J.Graham@software.ac.uk>
2018-10-25 10:12:31 +0100

    Clean MQTT test data script

--- mqtt.py
+++ mqtt.py
@@ -1,76 +1,55 @@
+#!/usr/bin/env python
 """
-this script writes proxy data to an mqtt broker
+This script writes test data to an MQTT broker
 """
 
+import datetime
+import random
 import threading
 import time
-import random
+
 import paho.mqtt.client as mqtt
 
-# mqtt broker
-host = "wonderbox.ecs.soton.ac.uk"
 
-# mqtt client id. Change this to something unique.
-client_name = "IAMACLIENT"
+def send_mqtt(client, mqtt_broker, log_path):
+    with open(log_path, "a") as lf:
+        while True:
+            lf.write("Posting data to " + mqtt_broker + " at " + str(time.time()) + "\n")
 
-logFile = "log.txt"
+            # Make up some data
+            # Syntax is important for str.split() in receiver.
+            temperatureReading = random.randint(0, 100)
+            humidityReading = random.randint(0, 100)
+            tempAndHumid = "Temperature: " + str(temperatureReading) + " Humidity: " + str(humidityReading)
+            print("Sending data " + "Temperature: " + str(temperatureReading) + " Humidity: " + str(humidityReading))
 
-#def on_message(client,userdata,message):
-    ##print("message topic=", message.topic)
-    ##print("message received ", str(message.payload.decode("utf-8")))
-    ## print("message qos=", message.qos)
-    ## print("message retain flag=", message.retain)
-    #outMsg = str(message.payload.decode("utf-8"))
-    #graph.data = outMsg
-    #return
+            windspeed = random.randint(0, 100)
+            rainfall = random.randint(0, 50)
+            wind_and_rain = "Windspeed: " + str(windspeed) + " Rainfall: " + str(rainfall)
+            wind = "Windspeed: " + str(windspeed) + " Timestamp: " + str(datetime.datetime.now())
+            print("Sending data " + "Windspeed: " + str(windspeed) + " Rainfall: " + str(rainfall))
 
-client = mqtt.Client(client_name)
-#client.on_message = on_message
-client.connect(host, port=1883, keepalive=60, bind_address="")
+            # Example publish setting
+            client.publish("datadump/tempAndHumid", tempAndHumid)
+            client.publish("datadump/windandrain", wind_and_rain)
+            client.publish("datadump/wind", wind)
 
-def send_mqtt():
-    while True:
-        lf = open(logFile,"a")
-        lf.write("Posting data to "+host+" at "+str(time.time()) + "\n")
-        lf.close()
+            time.sleep(5)
 
-        # make up some data, this should be sensors or whatever
-        # syntax is super important for str.split() not going tits up.
-        temperatureReading = random.randint(0,100)
-        humidityReading = random.randint(0,100)
-        tempAndHumid = "Temperature: " + str(temperatureReading) + " Humidity: " + str(humidityReading)
-        print("sending data "+"Temperature: " + str(temperatureReading) + " Humidity: " + str(humidityReading))
 
-        windspeed = random.randint(0,100)
-        rainfall = random.randint(0,50)
-        wind_and_rain = "Windspeed: " + str(windspeed) + " Rainfall: " + str(rainfall)
-        print("sending data "+"Windspeed: " + str(windspeed) + " Rainfall: " + str(rainfall))
+if __name__ == '__main__':
+    mqtt_broker = "wonderbox.ecs.soton.ac.uk"
+    client_name = "PtyDash Test Client"
+    log_path = "log.txt"
 
-        #example publish setting
-        client.publish("datadump/tempAndHumid",tempAndHumid)
-        client.publish("datadump/windandrain",wind_and_rain)
+    client = mqtt.Client(client_name)
+    client.connect(mqtt_broker, port=1883, keepalive=60, bind_address="")
 
-        time.sleep(5)
-    return
+    t_mqtt = threading.Thread(
+        target=send_mqtt,
+        args=(client,
+              mqtt_broker,
+              log_path)
+    )
+    t_mqtt.start()
 
-def write_data():
-
-    while True:
-        client.loop_start()
-        #example subscribe setting
-        client.subscribe("datadump/tempAndHumid")
-        client.subscribe("datadump/windandrain")
-        client.loop_stop()
-
-    return
-
-threads = []
-
-tMqtt = threading.Thread(target=send_mqtt)
-tWrite = threading.Thread(target=write_data)
-
-threads.append(tMqtt)
-threads.append(tWrite)
-
-tMqtt.start()
-tWrite.start()
